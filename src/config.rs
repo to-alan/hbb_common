d597d31389c99abb747133b93e55a2cfb2f02255
AlanChen <changetimor@gmail.com>
2025-04-05 01:41:46 +0800

    修改中级服务器域名和KEY

--- src/config.rs
+++ src/config.rs
@@ -98,11 +98,11 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.toalan.com"];
+pub const RS_PUB_KEY: &str = "s7afQn57LoY3wfriod1H4TcUVtsz6U4PF24KrHgWnR0=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
-pub const RELAY_PORT: i32 = 21117;
+pub const RELAY_PORT: i32 = 21119;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
